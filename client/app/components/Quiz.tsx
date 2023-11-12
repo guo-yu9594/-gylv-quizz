ee71e65cd1f5a5de3a19b6061bb259f6a65c419f
Guo Yu <guo.yu@epitech.eu>
2023-11-12 21:26:05 +0100

    feat: remove no needed element and etablish front base

---
+++ client/app/components/Quiz.tsx
@@ -0,0 +1,8 @@
+import { title, subtitle } from "@/components/primitives";
+
+export default function Quiz() {
+	return (
+		<section>
+		</section>
+	);
+}
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -41,18 +41,18 @@ export default function RootLayout({
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
 						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+						<main className="container mx-auto max-w-7xl pt-10 px-6 flex-grow">
 							{children}
 						</main>
 						<footer className="w-full flex items-center justify-center py-3">
 							<Link
 								isExternal
 								className="flex items-center gap-1 text-current"
-								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+								href={siteConfig.links.portfolio}
 								title="nextui.org homepage"
 							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
+								<span className="text-default-600">Made by</span>
+								<p className="text-primary">Guo Yu & Valentin LI</p>
 							</Link>
 						</footer>
 					</div>
