7696ff0dd02af101917e3105eadc8f8684cb5ebb
Guo Yu <guo.yu@epitech.eu>
2023-11-13 20:37:59 +0100

    feat: add quiz ending result display

--- client/app/components/Quiz.tsx
+++ client/app/components/Quiz.tsx
@@ -1,7 +1,25 @@
 "use client";
 
 import { title, subtitle, gradient } from "@/components/primitives";
-import { Card, Progress, Radio, RadioGroup, cn } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Link,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Progress,
+  RadioGroup,
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+  useDisclosure,
+} from "@nextui-org/react";
 import { defaultQuizData } from "@/config/data";
 import { useEffect, useState } from "react";
 import { CustomRadio } from "@/components/CustomRadio";
@@ -18,6 +36,11 @@ type PaletteType = {
     | "foreground";
 }[];
 
+type ResSelectType = {
+  res: number[];
+  select: string;
+};
+
 const palette: PaletteType = [
   { color: "primary", gradient: "blue" },
   { color: "secondary", gradient: "violet" },
@@ -26,22 +49,36 @@ const palette: PaletteType = [
   { color: "warning", gradient: "yellow" },
 ];
 
-const msPerQuestion: number = 15000;
+const msPerQuestion: number = 500;
 const progessRefreshTime: number = 50;
 
 export default function Quiz() {
+  const [showResult, setShowResult] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [clock, setClock] = useState(0);
+  const [responses, setResponses] = useState<ResSelectType>({
+    res: [],
+    select: "0",
+  });
   const currentColorIndex = currentQuestion % palette.length;
 
   useEffect(() => {
     const questionInterval = setInterval(() => {
       setCurrentQuestion((currentQuestion) => {
-        if (currentQuestion >= defaultQuizData.questions.length - 2)
+        if (currentQuestion >= defaultQuizData.questions.length - 1) {
           clearInterval(questionInterval);
-        return currentQuestion + 1;
+          clearInterval(clockInterval);
+          return currentQuestion;
+        } else {
+          setClock(0);
+          return currentQuestion + 1;
+        }
       });
-      setClock(0);
+      setResponses((current) => {
+        const newResponses = [...current.res, +current.select];
+        const newSelect = "0";
+        return { res: newResponses, select: newSelect };
+      });
     }, msPerQuestion);
     const clockInterval = setInterval(() => {
       setClock((clock) => clock + progessRefreshTime);
@@ -53,6 +90,12 @@ export default function Quiz() {
     };
   }, []);
 
+  useEffect(() => {
+    if (responses.res.length === defaultQuizData.questions.length)
+      setShowResult(true);
+  }, [responses]);
+
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
   return (
     <Card
       className={`w-full min-h-full p-8 space-y-12 justify-between`}
@@ -75,10 +118,12 @@ export default function Quiz() {
       <RadioGroup
         aria-label="fwefew"
         color={palette[currentColorIndex].color}
+        value={responses.select}
+        onChange={(e) => setResponses({ ...responses, select: e.target.value })}
       >
         {defaultQuizData.questions[currentQuestion].options.map((elem, idx) => {
           return (
-            <CustomRadio key={idx} aria-label={idx} value={elem}>
+            <CustomRadio key={idx} aria-label={idx} value={idx.toString()}>
               <h1
                 className={title({
                   color: palette[currentColorIndex].gradient,
@@ -91,6 +136,54 @@ export default function Quiz() {
           );
         })}
       </RadioGroup>
+      <Modal
+        isOpen={showResult}
+        backdrop="blur"
+        isDismissable={false}
+        hideCloseButton={true}
+        size="2xl"
+      >
+        <ModalContent>
+          {() => (
+            <>
+              <ModalHeader className="flex flex-col items-center pt-7">
+                <h1 className={title({ size: "md", color: "violet" })}>
+                  Quiz result
+                </h1>
+              </ModalHeader>
+              <ModalBody className="my-5">
+                <Table
+                  removeWrapper
+                  aria-label="Example static collection table"
+                >
+                  <TableHeader>
+                    <TableColumn>RANK</TableColumn>
+                    <TableColumn>NAME</TableColumn>
+                    <TableColumn>SCORE</TableColumn>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow key="1">
+                      <TableCell>1</TableCell>
+                      <TableCell>Tony Reichert</TableCell>
+                      <TableCell>11/15</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </ModalBody>
+              <ModalFooter className="flex flex-col items-center">
+                <Link href="/single">
+                  <Button color="primary">Replay</Button>
+                </Link>
+                <Link href="/">
+                  <Button color="primary" variant="light">
+                    Back to the menu
+                  </Button>
+                </Link>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </Card>
   );
 }
